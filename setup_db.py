fa79048607ce66275c5928709861303eb5299f00
Emilio Quezada <equezadanavarro@gmail.com>
2026-02-03 13:18:35 +0100

    Adding new moods to the list

--- setup_db.py
+++ setup_db.py
@@ -28,7 +28,7 @@ def setup():
             # Seed Moods (only if empty)
             if Mood.query.count() == 0:
                 print("Seeding initial moods 🌱 ...")
-                moods = ["Happy", "Sad", "Energetic", "Calm", "Anxious", "Focused", "Melancholic", "Excited"]
+                moods = ["Happy", "Sad", "Energetic", "Calm", "Anxious", "Focused", "Melancholic", "Excited", "Nostalgic", "Lonely", "Angry", "Frustrated", "Grateful", "Bored", "Confused"]
                 for label in moods:
                     db.session.add(Mood(label=label))
                 db.session.commit()
