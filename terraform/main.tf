9f02c1a951bcc648bf81a1bba5c97fb8a6e8de8e
Emilio Quezada <equezadanavarro@gmail.com>
2026-06-08 10:16:23 +0200

    Add route table and route table association

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,13 @@ terraform {
 }
 
 # Configure the AWS provider, setting the region for resource deployment.
+
 provider "aws" {
   region = "us-east-1"
 }
 
 # Resource 1: VPC
+# Private, isolated network in AWS where all resources live, nothing enters or exits without explicit permission.
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -24,6 +26,7 @@ resource "aws_vpc" "main" {
 
 
 # Resource 2: Public Subnet
+# A subdivision of the VPC whose traffic is routed to the Internet Gateway, making resources inside it (like EC2) reachable from the internet.
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
@@ -37,7 +40,33 @@ resource "aws_subnet" "main" {
 }
 
 # Resource 3: Internet gateway
+# Physical connection between VPC and the internet
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+}
+
+# Resource 4: Route Table
+# Tells traffic where to go
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+    
+  }
+
+  tags = {
+    Name = "Resonate-route-table"
+  }
+}
+
+# Resource 5: Route Table Association
+# Link the route table to your subnet
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
 }
